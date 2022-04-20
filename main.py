e048642437ad264f8209c5fe1a9061568fe0a09c
alex1114 <pokai.eed05@g2.nctu.edu.tw>
2022-04-20 16:22:46 +0800

    update listing delete

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import json
 import keep_alive
 import requests
 import asyncio
+# from faunadb import query as q
+# from faunadb.client import FaunaClient
 
-discord_token = os.environ['discord_token']
 osapikey = os.environ['osapikey']
+discord_token = os.environ['discord_token']
+
+# adminClient = FaunaClient(secret="")
 
 bot = commands.Bot(
 	command_prefix="k!",
@@ -20,7 +24,7 @@ async def on_ready():
   print("Ready!")
   while True:
     ################################################################################# 
-    # List event
+    # List event 
     #################################################################################
     l_url = "https://api.opensea.io/api/v1/events?asset_contract_address=0x88b7063107d2cf7fac83e30b37c938fe42c56040&event_type=created&only_opensea=false&limit=1"
     l_headers = {
@@ -31,6 +35,7 @@ async def on_ready():
     l_data = json.loads(l_response.text)
 
     l_name = l_data['asset_events'][0]['asset']['name']
+    l_id = l_data['asset_events'][0]['asset']["token_id"]
     #price
     l_starting_price = int(l_data['asset_events'][0]['starting_price'])/1000000000000000000
     #seller address
@@ -39,14 +44,13 @@ async def on_ready():
     l_permalink = l_data['asset_events'][0]['asset']['permalink']
     #image
     l_image_url = l_data['asset_events'][0]['asset']['image_url']
-
+    
     file = open("temp.json", "r")
     p = json.load(file)
     listed = p['listed']
-    file.close()
-
+    file.close()   
     ################################################################################# 
-    # Sold event
+    # Sold event  
     #################################################################################
     s_url = "https://api.opensea.io/api/v1/events?asset_contract_address=0x88b7063107d2cf7fac83e30b37c938fe42c56040&event_type=successful&only_opensea=false&limit=1"
 
@@ -59,6 +63,7 @@ async def on_ready():
 
     #NFT name
     s_name = s_data['asset_events'][0]['asset']['name']
+    s_id = s_data['asset_events'][0]['asset']["token_id"]
     #price
     s_starting_price = int(s_data['asset_events'][0]['total_price'])/1000000000000000000
     #seller address
@@ -76,7 +81,7 @@ async def on_ready():
     file.close()
 
     ################################################################################# 
-    # Embed
+    # Embed and Faunadb 
     #################################################################################
     if listed != l_name:
       jsonObject = {
@@ -87,7 +92,7 @@ async def on_ready():
       json.dump(jsonObject, file)
       file.close()
 
-      channel=bot.get_channel(914526949630693458)
+      channel=bot.get_channel(944846007051620404)
       embed=discord.Embed(title=" ", description=" ", color=0xe8006f)
       embed.set_author(name=F"[list] {l_name}")
       embed.set_image(url=l_image_url)
@@ -95,6 +100,14 @@ async def on_ready():
       embed.add_field(name="seller", value=f"{l_seller_address}", inline=False) 
       embed.add_field(name="OpenSea", value=f"{l_permalink}", inline=False) 
       await channel.send(embed=embed)
+
+      #listing tracking (faunadb)
+      # try:
+      #   result = adminClient.query(q.delete(q.ref(q.collection("Listing-Tracker"), l_id)))
+      # except Exception as e:
+      #   result = e
+      # print(result)
+    
     await asyncio.sleep(20)
 
     if sold != s_name:
@@ -106,7 +119,7 @@ async def on_ready():
       json.dump(jsonObject, file)
       file.close()
 
-      channel=bot.get_channel(914526949630693458)
+      channel=bot.get_channel(944846007051620404)
       embed=discord.Embed(title=" ", description=" ", color=0x00ff00)
       embed.set_author(name=F"[sold] {s_name}")
       embed.set_image(url=s_image_url)
@@ -115,6 +128,14 @@ async def on_ready():
       embed.add_field(name="buyer", value=f"{s_buyer_address}", inline=False) 
       embed.add_field(name="OpenSea", value=f"{s_permalink}", inline=False) 
       await channel.send(embed=embed)
+      
+      #listing tracking (faunadb)
+      # try:
+      #   result = adminClient.query(q.delete(q.ref(q.collection("Listing-Tracker"), s_id)))
+      # except Exception as e:
+      #   result = e
+      # print(result)
+      
     await asyncio.sleep(20) 
     #################################################################################
 
